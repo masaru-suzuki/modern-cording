d554eee5d19450e343cbc1730eb1d76f43bee50e
masaru-suzuki <m.suzuki@tam-tam.co.jp>
2022-02-24 14:13:58 +0900

    [feat]環境整備

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # lee-tec
 
 202203\_リーテック用リポジトリ
-test
+
+### コンパイル環境
+
+npm script
+
+### node version
+
+- バージョン管理: volta
+- node @16.14.0
---
+++ imagemin.js
@@ -0,0 +1,23 @@
+import mozjpeg from 'imagemin-mozjpeg';
+import keepfolder from 'imagemin-keep-folder';
+import pngquant from 'imagemin-pngquant';
+import gifsicle from 'imagemin-gifsicle';
+import svgo from 'imagemin-svgo';
+import webp from 'imagemin-webp';
+
+keepfolder(['src/img/**/*.{jpg,png,gif,svg}'], {
+  plugins: [
+    mozjpeg({
+      quality: 85,
+    }),
+    pngquant({
+      quality: [0.7, 0.8],
+    }),
+    gifsicle(),
+    svgo(),
+  ],
+  use: [webp({})],
+  replaceOutputDir: (output) => {
+    return output.replace(/img\//, '../dist/img/');
+  },
+});
